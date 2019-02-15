673e7286ef2e1d8441eef5b101ff0ba6174e7897
煜月(周龙)-技术中心 <yuyue@517na.com>
2019-02-15 14:35:20 +0800

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,21 +12,21 @@ module.exports = {
       path : '/node/pm2-deploy-demo',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env serverall'
     },
-    server182 : {
-      user : 'tomcat',
-      host : ['192.168.1.182'],
+    updateInstall : {
+      user : 'root',
+      host : ['101.132.46.123'],
       ref  : 'origin/master',
-      repo : 'git@github.com:MeedFine/pm2-deploy-demo.git',
+      repo : 'git@github.com:knife1/airTMC.git',
       path : '/node/pm2-deploy-demo',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env server182'
     },
-    server115 : {
-      user : 'tomcat',
-      host : ['192.168.1.115'],
+    update : {
+      user : 'root',
+      host : ['101.132.46.123'],
       ref  : 'origin/master',
-      repo : 'git@github.com:MeedFine/pm2-deploy-demo.git',
+      repo : 'git@github.com:knife1/airTMC.git',
       path : '/node/pm2-deploy-demo',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env server115'
+      'post-deploy' : 'pm2 reload ecosystem.config.js --env server115'
     },
   }
 };
